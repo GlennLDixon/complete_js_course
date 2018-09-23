3dddf9cf96984bd287ca1da66c28fa3f26735daa
Glenn Dixon <glenndixon19@gmail.com>
2018-09-23 12:42:03 -0500

    added comments and commented out previous code

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,3 +1,8 @@
+/***************************
+  Variables and data types
+***************************/
+
+/*
 var firstName = 'Lester';
 console.log(firstName);
 
@@ -13,6 +18,17 @@ console.log(job);
 job = 'Teacher';
 console.log(job);
 
+// Variable Name Rules
 var $3years = 3;
 var johnMark = "John and Mark";
 var if = 23;
+*/
+
+/***************************
+  Variable mutation and type coercion
+***************************/
+
+var firstName = 'John';
+var age = 28;
+
+console.log(firstName + ' ' + age);
