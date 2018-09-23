0ca918c0fc94c88dd80510b05dd8648211351a58
Glenn Dixon <glenndixon19@gmail.com>
2018-09-23 12:01:06 -0500

    understand variables and datatypes

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,14 @@
+var firstName = 'Lester';
+console.log(firstName);
+
+var lastName = 'Lane';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
