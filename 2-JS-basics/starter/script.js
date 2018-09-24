50a90ef7bbeeff5dfa0e30f7a57d64e5cf994ecb
Glenn Dixon <glenndixon19@gmail.com>
2018-09-24 14:09:19 -0500

    basic js operators

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -28,6 +28,7 @@ var if = 23;
   Variable mutation and type coercion
 ***************************/
 
+/*
 var firstName = 'Lester';
 var age = 28;
 
@@ -48,11 +49,37 @@ alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + is
 
 var lastName = prompt('What is his last Name?')
 console.log(firstName + ' ' + lastName);
+*/
 
+/***************************
+  Basic Operators
+***************************/
+var year, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
 
+// Math operators
+year = 2018;
+yearJohn = year - ageJohn;
+yearMark = year - ageMark;
 
+console.log(yearJohn);
 
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
 
+//Logical operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+//typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+var x;
+console.log(typeof x);
 
 
 
