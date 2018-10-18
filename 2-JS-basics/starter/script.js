d5823a58a34129fac351db9be83d10fca7358c69
Glenn Dixon <glenndixon19@gmail.com>
2018-10-17 20:07:46 -0500

    truthy and falsy values and equality operators

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -233,3 +233,45 @@ switch (job) {
       console.log(firstName + ' is a man.');
   }
 */
+
+/**********************************************
+Truthy and Falsy values and equality operators
+**********************************************/
+
+// falsy values: undefined, null, 0, '', NaN
+// truthy values: NOT falsy values
+
+var height;
+
+height = 23;
+
+if (height || height === 0) {
+    console.log('variable is defined');
+} else {
+  console.log('variable has NOT been defined');
+}
+
+// Equality operators
+if (height == '23') {
+  console.log('The == operator does type coercion');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
